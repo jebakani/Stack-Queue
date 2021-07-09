97aa522b042bce6f8bc84e11fc09342f664a071a
Jebakani <vjishwarya@gmail.com>
2021-07-09 20:41:10 +0530

    [JEBAKANI] UC1-creating the push method

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -7,6 +7,11 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to stack and Queue");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(53);
+            Console.WriteLine("Stack is created and elements are pushed");
         }
     }
 }
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackNode<T>
+    {
+        public T value;
+        public StackNode<T> Next;
+        public StackNode(T value)
+        {
+            this.value = value;
+            this.Next = null;
+        }
+    }
+    public class Stack<T>
+    {
+        public StackNode<T> top;
+
+        public void Push(T value)
+        {
+            StackNode<T> stackNode = new StackNode<T>(value);
+            if(top==null)
+            {
+                top = stackNode;
+            }
+            else
+            {
+                stackNode.Next = top;
+                top = stackNode;
+            }
+        }
+
+    }
+}
